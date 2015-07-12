b5857b501cc4b41b4aa017abe6182ae73d35900c
Joe Trovato <joetrovato@Joes-MacBook-Pro.local>
2015-07-12 12:43:54 -0600

    median word with heaps works

--- src/median_unique.py
+++ src/median_unique.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import optparse
 import sys
+import heapq
 
 #Python script to track the median unique number of words in a stream of tweets
 
@@ -9,24 +10,24 @@ This function uses a min heap and a max heap to keep track of the middle two or 
 from these numbers the median can easily be found based on the number of elements in each heap
 '''
 def calcMedianHeaps(word_list, min_heap, max_heap):
-    cur_num_words = len(unique(word_list))
+    cur_num_words = len(set(word_list)) # use set to delete repeated words
     # add the current number to one of the heaps
-    if cur_num_words < max_heap[0]:
-    	max_heap._push_max(cur_num_words)
+    if len(max_heap) == 0 or cur_num_words < max_heap[0]:
+    	heapq.heappush(max_heap, -cur_num_words)
     else:
-    	min_heap.push(cur_num_words)
+    	heapq.heappush(min_heap, cur_num_words)
     #adjust the heaps so that min and max are balanced
-    if len(max_heap) - len(min_heap) > 1
-    	min_heap.push(max_heap._pop_max())
-    elif: len(min_heap) - len(max_heap) > 1
-    	max_heap._push_max(min_heap.pop())
+    if len(max_heap) - len(min_heap) > 1:
+    	heapq.heappush(min_heap, -heapq.heappop(max_heap))
+    elif len(min_heap) - len(max_heap) > 1:
+    	heapq.heappush(max_heap, -heapq.heappop(min_heap))
     #find the median
     if len(min_heap) == len(max_heap):
-        median = (min_heap[0] + max_heap[0])/2
+        median = (min_heap[0] + -max_heap[0])/2.0
     elif len(min_heap) > len(max_heap):
         median = min_heap[0]
     elif len(max_heap) > len(min_heap):
-        median = max_heap[0]
+        median = -max_heap[0]
 
     return median, min_heap, max_heap
     
@@ -43,8 +44,8 @@ if __name__ == '__main__':
     max_heap = []
 
     # read file line by line calculating the median at each step
-    with open(input_filepath,'r'), open(output_filepath, 'w') as infile, outfile:
+    with open(input_filepath,'r') as infile, open(output_filepath, 'w') as outfile:
         for line in infile:
             word_list = line.split()
-            median, min_heap, max_heap = calcMedianHeaps(tweet_dict, word_list)
+            median, min_heap, max_heap = calcMedianHeaps(word_list, min_heap, max_heap)
             outfile.write(str(median) + '\n')
